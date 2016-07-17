c6b9747a92bc01c88725a1e3566641c447c6f5f6
Jayce Ardon <Jayce.ardon@gmail.com>
2016-07-17 16:34:47 +0200

    fixed scroll2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
 	// javascript
+    $("#sidebar-nav li").on("click", function(e){
+        e.preventDefault();
+
+
+        console.log();
+
+        $("#sidebar-nav").find("li.active").removeClass("active");
+        $(this).addClass("active");
+
+        var link = $($(this).find("a"));
+
+        $('html, body').animate({
+            scrollTop: $(link.attr("href")).offset().top
+        }, 500);
+
+    })
 
 });
